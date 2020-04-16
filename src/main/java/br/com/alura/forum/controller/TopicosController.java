df25f86d64db011bd6d69944074ae339839bee87
douglaslbittencourt <douglaslbittencourt@gmail.com>
2020-04-16 15:07:34 -0300

    Aula 03

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,30 @@
 package br.com.alura.forum.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	@ResponseBody
-	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Jojo", "Johnny Joestar", new Curso("Stand", "Tusk"));
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	public List<TopicoDto> lista(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
 	}
 }
